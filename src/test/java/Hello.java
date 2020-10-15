a1f61666cc1de307bca3e70e2c4cd6b1533e2987
hysnn <hsynn03@gmail.com>
2020-10-15 13:53:05 +0300

    adding new line of comment locally not pushed

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("this is my own hello");// this is my local hello, and commit
+        //adding some local change
     }
 }

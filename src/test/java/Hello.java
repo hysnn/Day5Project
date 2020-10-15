f3bb5382e751380568bbc0dbc3ad679f96b0be85
hysnn <66172680+hysnn@users.noreply.github.com>
2020-10-15 13:53:56 +0300

    co-worker done more changes

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("hello from co worker");// we are simulating the situation my co worker made this change
+        //co-worker make new changes
     }
 }

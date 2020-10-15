cf38019b2539fcfb4923691555191d21b085d52e
hysnn <hsynn03@gmail.com>
2020-10-15 11:12:28 +0300

    feature 2 more commit

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -1,5 +1,8 @@
 public class Feature2 {
     public static void main(String[] args) {
         System.out.println("working on feature2");
+        System.out.println("working on feature2 again");
+        System.out.println("working on feature2 more");
+
     }
 }

74266623e36aaf358d3a88bc3bac925a40b2d574
hysnn <hsynn03@gmail.com>
2020-10-15 12:10:08 +0300

    feature 2 more changes to line 3

--- src/test/java/Feature2.java
+++ src/test/java/Feature2.java
@@ -4,5 +4,6 @@ public class Feature2 {
         System.out.println("working on feature2 again");
         System.out.println("working on feature2 more");
 
+        System.out.println("adding some more changes to line 3");
     }
 }

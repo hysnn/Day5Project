0237207278a5dc6959221b89ea6e3edd5775b397
hysnn <hsynn03@gmail.com>
2020-10-15 12:52:12 +0300

    added comment on same line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");
-        // added one more line on my local
+        System.out.println("hello");// added one more line on my local
     }
 }

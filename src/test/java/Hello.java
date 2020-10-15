90cbe0ffbbd0910b51d0a103aa6ec23abb11bc57
hysnn <hsynn03@gmail.com>
2020-10-15 13:48:35 +0300

    added some local line

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello");// added one more line on my local
+        System.out.println("this is my own hello");// this is my local hello, and commiting
     }
 }
